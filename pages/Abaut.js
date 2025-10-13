8f8a11d94d5faf7a15e879ca8fb85a8a9a835cbe
FanWiki <148832529+Mister-c21@users.noreply.github.com>
2025-10-12 23:22:23 -0300

    Create Abaut.js

---
+++ pages/Abaut.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Lógica dos Dropdowns
+    const dropdownHeaders = document.querySelectorAll(".dropdown-header");
+    dropdownHeaders.forEach(header => {
+        header.addEventListener("click", function() {
+            this.classList.toggle("active");
+            const content = this.nextElementSibling;
+            if (content.classList.contains("show")) {
+                content.classList.remove("show");
+                content.style.maxHeight = null;
+                content.style.padding = "0 20px";
+            } else {
+                content.classList.add("show");
+                content.style.maxHeight = content.scrollHeight + "px";
+                content.style.padding = "15px 20px";
+            }
+        });
+    });
+});
