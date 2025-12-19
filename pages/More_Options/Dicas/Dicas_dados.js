78c23ef9753762d5ae4d5d1123418e658dabb0d5
FanWiki <148832529+Mister-c21@users.noreply.github.com>
2025-12-19 18:21:02 -0300

    Create Dicas_dados.js

---
+++ pages/More_Options/Dicas/Dicas_dados.js
@@ -0,0 +1,22 @@
+const DICAS_DATA = [
+    { 
+        text: "Ative o 'XMP' na BIOS para extrair a velocidade real da sua memória RAM.", 
+        cat: "games", 
+        link: "https://www.google.com/search?q=como+ativar+xmp+bios" 
+    },
+    { 
+        text: "Pressione 'Windows + V' para gerenciar tudo o que você copiou recentemente no histórico.", 
+        cat: "tech", 
+        link: "https://support.microsoft.com/pt-br/windows/ajuda-na-%C3%A1rea-de-transfer%C3%AAncia-no-windows-v" 
+    },
+    { 
+        text: "Para um arroz perfeito, use sempre o dobro de água para cada medida de arroz.", 
+        cat: "culinaria", 
+        link: "https://receitas.globo.com/arroz-branco-soltinho.ghtml" 
+    },
+    { 
+        text: "Limpe vidros com jornal e vinagre para um brilho sem manchas.", 
+        cat: "vida", 
+        link: "https://www.tuacasa.com.br/como-limpar-vidros/" 
+    }
+];
