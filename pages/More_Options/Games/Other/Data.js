d4dc70410030621e768ba5d31131239c1d102158
FanWiki <148832529+Mister-c21@users.noreply.github.com>
2025-12-21 21:10:47 -0300

    Create Data.js

---
+++ pages/More_Options/Games/Other/Data.js
@@ -0,0 +1,27 @@
+const database = [
+    { 
+        t: "Mega Man X", 
+        img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2049.jpg", 
+        play: "https://www.retrogames.cc/embed/42276-mega-man-x3-zero-project-v4-1.html" 
+    },
+    { 
+        t: "Mega Man Zero", 
+        img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1w3n.jpg", 
+        play: "https://www.retrogames.cc/embed/11051-mega-man-zero.html" 
+    },
+    { 
+        t: "Castlevania: SotN", 
+        img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1r7f.jpg", 
+        play: "https://www.retrogames.cc/embed/41721-castlevania-symphony-of-the-night.html" 
+    },
+    { 
+        t: "Sonic CD", 
+        img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co28bi.jpg", 
+        play: "https://www.retrogames.cc/embed/30413-sonic-cd.html" 
+    },
+    { 
+        t: "Super Metroid", 
+        img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1v65.jpg", 
+        play: "https://www.retrogames.cc/embed/11993-super-metroid.html" 
+    }
+];
