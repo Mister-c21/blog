67b13bc799cd18364d7462620e2c0edaa1c7c5be
FanWiki <148832529+Mister-c21@users.noreply.github.com>
2026-01-02 09:25:08 -0300

    Create dados.js

---
+++ pages/More_Options/Games/Others_Games/dados.js
@@ -0,0 +1,36 @@
+// dados.js - Repositório de links e mídias
+export const loreData = [
+    { 
+        title: "Manuscritos de Éter", 
+        img: "https://images.unsplash.com/photo-1542751371-adc38448a05e?w=500", 
+        url: "lore_exemplo_1.html" 
+    },
+    { 
+        title: "O Código de Neon", 
+        img: "https://images.unsplash.com/photo-1614850523296-d8c1af93d400?w=500", 
+        url: "lore_exemplo_2.html" 
+    }
+];
+
+export const retroGames = [
+    { 
+        title: "ben 10", 
+        url: "https://www.retrogames.cc/embed/42019-ben-10-omniverse-usa-en-fr-es.html" 
+    },
+    { 
+        title: "Sonic the Hedgehog", 
+        url: "https://retrogame.cc/embed/sonic-the-hedgehog.html" 
+    },
+    { 
+        title: "Mega Man X", 
+        url: "https://retrogame.cc/embed/mega-man-x.html" 
+    },
+    { 
+        title: "Street Fighter II", 
+        url: "https://retrogame.cc/embed/street-fighter-ii-the-world-warrior.html" 
+    },
+    { 
+        title: "The Legend of Zelda", 
+        url: "https://retrogame.cc/embed/the-legend-of-zelda-a-link-to-the-past.html" 
+    }
+];
