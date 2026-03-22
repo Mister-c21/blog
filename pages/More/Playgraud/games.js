d26763ee2182dfd7dc2967cf6128d9ae72dfc33b
FanWiki <148832529+Mister-c21@users.noreply.github.com>
2026-03-22 18:13:26 -0300

    Create games.js

---
+++ pages/More/Playgraud/games.js
@@ -0,0 +1,40 @@
+// games.js
+export const consoles = {
+    "Populares no GBA": { 
+        key: "GBA", 
+        games: [
+            { title: "Sonic Advance", url: "https://www.retrogames.cc/embed/28783-sonic-advance-j-independent.html" },
+            { title: "Sonic Advance 2", url: "https://www.retrogames.cc/embed/28169-sonic-advance-2-j-eurasia.html" },
+            { title: "Sonic Advance 3", url: "https://www.retrogames.cc/embed/19452-sonic-advance-3-e-trashman.html" },
+            { title: "Sonic Battle", url: "https://www.retrogames.cc/embed/26763-sonic-battle-j-eurasia.html" },
+            { title: "Mega Man Zero", url: "https://www.retrogames.cc/embed/19499-megaman-zero-u-venom.html" },
+            { title: "Mega Man Zero 2", url: "https://www.retrogames.cc/embed/26254-megaman-zero-2-e-patience.html" },
+            { title: "Mega Man Zero 3", url: "https://www.retrogames.cc/embed/19493-megaman-zero-3-e-rising-sun.html" },
+            { title: "Mega Man Zero 4", url: "https://www.retrogames.cc/embed/28027-megaman-zero-4-e-supplex.html" }
+        ]
+    },
+    "Ação no Nintendo DS": { 
+        key: "NDS", 
+        games: [
+            { title: "Ben 10: Protector of Earth", url: "https://www.retrogames.cc/embed/36951-ben-10-protector-of-earth-europe-en-fr-de-es-it.html" },
+            { title: "Ben 10: Alien Force", url: "https://www.retrogames.cc/embed/38112-ben-10-alien-force-usa-en-fr-de-es-it-rev-1.html" },
+            { title: "Ben 10: Alien Force - Vilgax Attacks", url: "https://www.retrogames.cc/embed/39567-ben-10-alien-force-vilgax-attacks-europe-en-fr-de-es-it.html" },
+            { title: "Ben 10 Ultimate Alien: Cosmic Destruction", url: "https://www.retrogames.cc/embed/42018-ben-10-ultimate-alien-cosmic-destruction-usa-en-fr-de-es-it.html"},
+            { title: "Ben 10: Galactic Racing", url: "https://www.retrogames.cc/embed/46431-ben-10-galactic-racing-usa-en-fr-de-es-it.html" },
+            { title: "Ben 10: Omniverse", url: "https://www.retrogames.cc/embed/42019-ben-10-omniverse-usa-en-fr-es.html" }
+        ]
+    },
+    "Clássicos do SNES": { 
+        key: "SNES", 
+        games: [
+            { title: "Mega Man X", url: "https://www.retrogames.cc/embed/18705-megaman-x-usa-rev-a.html" },
+            { title: "Mega Man X2", url: "https://www.retrogames.cc/embed/19996-megaman-x2-usa.html" },
+            { title: "Mega Man X3", url: "https://www.retrogames.cc/embed/22454-megaman-x3-europe.html" },
+            { title: "Mega Man 7", url: "https://www.retrogames.cc/embed/16692-megaman-vii-usa.html" },
+            { title: "Mega Man Soccer", url: "https://www.retrogames.cc/embed/22454-megaman-x3-europe.html" },
+            { title: "Mega Man & Bass", url: "https://www.retrogames.cc/embed/43955-megaman-bass.html" }
+        ]
+    }
+};
+
+
