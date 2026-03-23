77411139c4cb761e7d5d10eaa67144554d2f9ddf
FanWiki <148832529+Mister-c21@users.noreply.github.com>
2026-03-22 22:19:26 -0300

    Update games.js

--- pages/More/Playgraud/games.js
+++ pages/More/Playgraud/games.js
@@ -7,16 +7,29 @@ export const consoles = {
             { title: "Sonic Advance 2", url: "https://www.retrogames.cc/embed/28169-sonic-advance-2-j-eurasia.html" },
             { title: "Sonic Advance 3", url: "https://www.retrogames.cc/embed/19452-sonic-advance-3-e-trashman.html" },
             { title: "Sonic Battle", url: "https://www.retrogames.cc/embed/26763-sonic-battle-j-eurasia.html" },
+            { title:"Sonic Pinball Party", url: "https://www.retrogames.cc/embed/19365-sonic-pinball-party-u-venom.html"},
+            { title:"Sonic The Hedgehog - Genesis", url: "https://www.retrogames.cc/embed/27910-sonic-the-hedgehog-genesis-u-trashman.html"},
             { title: "Mega Man Zero", url: "https://www.retrogames.cc/embed/19499-megaman-zero-u-venom.html" },
             { title: "Mega Man Zero 2", url: "https://www.retrogames.cc/embed/26254-megaman-zero-2-e-patience.html" },
             { title: "Mega Man Zero 3", url: "https://www.retrogames.cc/embed/19493-megaman-zero-3-e-rising-sun.html" },
             { title: "Mega Man Zero 4", url: "https://www.retrogames.cc/embed/28027-megaman-zero-4-e-supplex.html" },
+            { title:"MegaMan Battle Network", url: "https://www.retrogames.cc/embed/27902-megaman-battle-network-e-rocket.html"},
+            { title:"MegaMan Battle Network 2", url: "https://www.retrogames.cc/embed/26281-megaman-battle-network-2-u-mode7.html"},
+            { title:"MegaMan Battle Network 3 ", url: "https://www.retrogames.cc/embed/27716-megaman-battle-network-3-white-version-u-mode7.html"},
+            { title:"MegaMan Battle Network 4 ", url: "https://www.retrogames.cc/embed/26877-megaman-battle-network-4-red-sun-u-independent.html"},
+            { title:"Megaman Battle Network 5", url: "https://www.retrogames.cc/embed/26570-megaman-battle-network-5-team-protoman-u-trashman.html"},
+            { title:"MegaMan Battle Network 6 ", url: "https://www.retrogames.cc/embed/27799-megaman-battle-network-6-cybeast-falzar-u-independent.html"},
+            { title:"Megaman Battle Chip Challenge", url: "https://www.retrogames.cc/embed/29094-megaman-battle-chip-challenge-e-rising-sun.html"},
             { title:"Dragonball Advanced Adventure", url:"https://www.retrogames.cc/embed/26252-dragonball-advanced-adventure-e-rising-sun.html"},
             { title:"Dragon Ball Z: Supersonic Warriors", url: "https://www.retrogames.cc/embed/19537-dragonball-z-supersonic-warriors-e-rising-sun.html"},
             { title:"Dragon Ball Z: The Legacy of Goku", url: "https://www.retrogames.cc/embed/26814-dragon-ball-z-the-legacy-of-goku-e-polla.html"},
             { title:"Dragon Ball Z - The Legacy of Goku II", url: "https://www.retrogames.cc/embed/27814-dragon-ball-z-the-legacy-of-goku-ii-e-eurasia.html"},
             { title:"Dragon Ball Z - Buu's Fury", url: "https://www.retrogames.cc/embed/28935-dragon-ball-z-buu-s-fury-u-psychosis.html"},
-            { title:"Dragon Ball GT: Transformation", url: "https://www.retrogames.cc/embed/27773-dragon-ball-gt-transformation-u-trashman.html"}
+            { title:"Dragon Ball GT: Transformation", url: "https://www.retrogames.cc/embed/27773-dragon-ball-gt-transformation-u-trashman.html"},
+            { title:"Pokemon Ephemerald", url: "https://www.retrogames.cc/embed/42901-pokemon-ephemerald-v1-0.html"},
+            { title:"Pokemon Fire Red", url: "https://www.retrogames.cc/embed/42901-pokemon-ephemerald-v1-0.html"},
+            { title:"Kirby: The Amazing Mirror", url: "https://www.retrogames.cc/embed/19385-kirby-and-the-amazing-mirror-e-rising-sun.html"},
+            { title:"Kirby: Nightmare in Dreamland ", url: "https://www.retrogames.cc/embed/26683-kirby-nightmare-in-dreamland-u-mode7.html"},
         ]
     },
     "Ação no Nintendo DS": { 
@@ -27,7 +40,16 @@ export const consoles = {
             { title: "Ben 10: Alien Force - Vilgax Attacks", url: "https://www.retrogames.cc/embed/39567-ben-10-alien-force-vilgax-attacks-europe-en-fr-de-es-it.html" },
             { title: "Ben 10 Ultimate Alien: Cosmic Destruction", url: "https://www.retrogames.cc/embed/42018-ben-10-ultimate-alien-cosmic-destruction-usa-en-fr-de-es-it.html"},
             { title: "Ben 10: Galactic Racing", url: "https://www.retrogames.cc/embed/46431-ben-10-galactic-racing-usa-en-fr-de-es-it.html" },
-            { title: "Ben 10: Omniverse", url: "https://www.retrogames.cc/embed/42019-ben-10-omniverse-usa-en-fr-es.html" }
+            { title: "Ben 10: Omniverse", url: "https://www.retrogames.cc/embed/42019-ben-10-omniverse-usa-en-fr-es.html" },
+            { title:"Dragon Ball Z: Supersonic Warriors 2", url:"https://www.retrogames.cc/embed/35336-dragon-ball-z-supersonic-warriors-2-usa.html"},
+            { title:"Dragon Ball: Origins", url:"https://www.retrogames.cc/embed/38014-dragon-ball-origins-usa.html"},
+            { title:"Dragon Ball: Origins 2", url:"https://www.retrogames.cc/embed/43080-dragon-ball-origins-2-usa-en-fr-es.html"},
+            { title:"Dragon Ball Z: Attack of the Saiyans", url:"https://www.retrogames.cc/embed/39503-dragon-ball-z-attack-of-the-saiyans-europe-en-fr-de-es-it.html"},
+            { title:"Dragon Ball DS", url:"https://www.retrogames.cc/embed/37814-dragon-ball-ds-japan.html"},
+            { title:"Dragon Ball DS 2", url:"https://www.retrogames.cc/embed/39851-dragon-ball-ds-2-totsugeki-red-ribbon-gun-japan.html"},
+            { title:"Power Rangers: Samurai", url:"https://www.retrogames.cc/embed/46620-power-rangers-samurai-usa-en-fr-es.html"},
+            { title:"Power Rangers: Super Legends", url:"https://www.retrogames.cc/embed/36677-power-rangers-super-legends-usa-en-fr-de-es-it.html"},
+            { title:"Metroid Prime: Hunters", url:"https://www.retrogames.cc/embed/35570-metroid-prime-hunters-europe-en-fr-de-es-it.html"},
         ]
     },
     "Clássicos do SNES": { 
@@ -38,12 +60,34 @@ export const consoles = {
             { title: "Mega Man X3", url: "https://www.retrogames.cc/embed/22454-megaman-x3-europe.html" },
             { title: "Mega Man 7", url: "https://www.retrogames.cc/embed/16692-megaman-vii-usa.html" },
             { title: "Mega Man Soccer", url: "https://www.retrogames.cc/embed/22454-megaman-x3-europe.html" },
-            { title: "Mega Man & Bass", url: "https://www.retrogames.cc/embed/43955-megaman-bass.html" }
+            { title: "Mega Man & Bass", url: "https://www.retrogames.cc/embed/43955-megaman-bass.html" },
+            { title:"Marvel Super Heroes: War of the Gems", url: "https://www.retrogames.cc/embed/22520-marvel-super-heroes-war-of-the-gems-usa.html"},
+            { title:"Mickey's Playtown Adventure: A Day of Discovery!", url: "https://www.retrogames.cc/embed/24180-mickey-s-playtown-adventure-a-day-of-discovery-usa-proto.html"},
+            { title:"Mickey to Minnie: Magical Adventure 2", url: "https://www.retrogames.cc/embed/19633-mickey-to-minnie-magical-adventure-2-japan.html"},
+            { title:"Mickey to Donald: Magical Adventure 3", url: "https://www.retrogames.cc/embed/19646-mickey-to-donald-magical-adventure-3-japan-en-by-rpgone-v1-1-mickey-donald-magical-adventure-3.html"},
+            { title:"Mighty Morphin Power Rangers - The Movie", url: "https://www.retrogames.cc/embed/19560-mighty-morphin-power-rangers-the-movie-usa.html"},
+            { title:"Mighty Morphin Power Rangers", url: "https://www.retrogames.cc/embed/24474-mighty-morphin-power-rangers-europe.html"},
+            { title:"Mighty Morphin Power Rangers: The Fighting Edition", url: "https://www.retrogames.cc/embed/23323-mighty-morphin-power-rangers-the-fighting-edition-usa.html"},
+        ]
+    },
+    
+    "Clássicos do Ps1": { 
+        key: "PS1", 
+        games: [
+            { title: "Mega Man 8", url: "https://www.retrogames.cc/embed/40781-mega-man-8.html" },
+            { title:"Mega Man: Battle & Chase", url: "https://www.retrogames.cc/embed/40782-mega-man-battle-chase.html"},
+            { title: "Mega Man X4", url: "https://www.retrogames.cc/embed/40778-mega-man-x4.html" },
+            { title: "Mega Man X5", url: "https://www.retrogames.cc/embed/40779-mega-man-x5.html" },
+            { title: "Mega Man X6", url: "https://www.retrogames.cc/embed/40780-mega-man-x6.html" },
+            { title: "Mega Man Legends", url: "https://www.retrogames.cc/embed/40783-mega-man-legends.html" },
+            { title: "Mega Man Legends 2", url: "https://www.retrogames.cc/embed/42739-mega-man-legends-2.html" },
+            { title:"Dragon Ball GT - Final Bout", url: "https://www.retrogames.cc/embed/40092-dragon-ball-gt-final-bout.html"},
+            { title:"Dragon Ball Z: The Legend", url: "https://www.retrogames.cc/embed/40793-dragon-ball-z-the-legend.html"},
+            { title:"Dragon Ball Z: Ultimate Battle 22", url: "https://www.retrogames.cc/embed/40794-dragon-ball-z-ultimate-battle-22.html"},
+            { title:"Digimon World", url:"https://www.retrogames.cc/embed/44551-digimon-world-usa.html"},
         ]
     }
+    
 };
-
-
-
 
 
