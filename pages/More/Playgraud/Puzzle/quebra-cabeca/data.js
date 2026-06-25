2d91d7b852cd7b1c0e24e2e78cedf41844a96554
FanWiki <148832529+Mister-c21@users.noreply.github.com>
2026-06-25 00:58:11 -0300

    Create data.js

---
+++ pages/More/Playgraud/Puzzle/quebra-cabeca/data.js
@@ -0,0 +1,5 @@
+const webImages = [
+    { title: 'Natureza', url: 'https://wallpaperaccess.com/full/2212356.jpg' },
+    { title: 'Espaço', url: 'https://wallpaperaccess.com/full/2212357.jpg' },
+    { title: 'Abstrato', url: 'https://wallpaperaccess.com/full/2212358.jpg' }
+];
