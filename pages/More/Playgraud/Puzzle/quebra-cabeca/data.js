8072a1e94519110a47ba563e504a3c3cd91f05df
FanWiki <148832529+Mister-c21@users.noreply.github.com>
2026-06-25 02:25:24 -0300

    Fix syntax error in data.js for webImages

--- pages/More/Playgraud/Puzzle/quebra-cabeca/data.js
+++ pages/More/Playgraud/Puzzle/quebra-cabeca/data.js
@@ -1,5 +1,5 @@
 const webImages = [
-    { title: 'miraculos', url': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNl8WU1PJeZ_uwpDvZ5sJYEkV84NEGe9jO4cIrClNEGeQdJsTI2OmL2CKx&s=10' },
+    { title: 'miraculos', url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNl8WU1PJeZ_uwpDvZ5sJYEkV84NEGe9jO4cIrClNEGeQdJsTI2OmL2CKx&s=10' },
     { title: 'sonic 3', url: 'https://www.ludlowcub.com/wp-content/uploads/2025/01/IMG_4074-966x1200.jpg' },
     { title: 'sonic 2', url: 'https://a.storyblok.com/f/178900/614x768/d6c903177e/1ba1837329c2662f0f9eb2c0d7a34acc1647265404_main.jpg/m/filters:quality(95)format(webp)' },
     { title: 'sonic', url: 'https://blogdomatteusboni.wordpress.com/wp-content/uploads/2020/03/d8534ef914bab938d35daa14b8141715.jpg' },
