44517ea9e6a45633363d521978ea51ccf1774895
FanWiki <148832529+Mister-c21@users.noreply.github.com>
2026-06-25 02:12:05 -0300

    Fix syntax error in webImages array

--- pages/More/Playgraud/Puzzle/quebra-cabeca/data.js
+++ pages/More/Playgraud/Puzzle/quebra-cabeca/data.js
@@ -1,5 +1,5 @@
 const webImages = [
-    { title: 'Miraculos, url': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNl8WU1PJeZ_uwpDvZ5sJYEkV84NEGe9jO4cIrClNEGeQdJsTI2OmL2CKx&s=10' },
+    { title: 'Miraculos', url': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNl8WU1PJeZ_uwpDvZ5sJYEkV84NEGe9jO4cIrClNEGeQdJsTI2OmL2CKx&s=10' },
     { title: 'sonic 3', url: 'https://www.ludlowcub.com/wp-content/uploads/2025/01/IMG_4074-966x1200.jpg' },
     { title: 'sonic 2', url: 'https://a.storyblok.com/f/178900/614x768/d6c903177e/1ba1837329c2662f0f9eb2c0d7a34acc1647265404_main.jpg/m/filters:quality(95)format(webp)' },
     { title: 'sonic 2', url: 'https://wallpaperaccess.com/full/2212358.jpg' }
