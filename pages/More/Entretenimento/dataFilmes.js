d346f0fd79a66b5cf871ac09388c0884815dba76
FanWiki <148832529+Mister-c21@users.noreply.github.com>
2026-07-04 00:11:18 -0300

    Update dataFilmes.js

--- pages/More/Entretenimento/dataFilmes.js
+++ pages/More/Entretenimento/dataFilmes.js
@@ -3,7 +3,7 @@ const dataFilmes = [
     { t: 'A Lenda Da Serpente', v: 'pjyih9cmfKk' },
     { t: 'Operação Zodíaco', v: 'EPZzk-H0iew'},
     { t: 'Kung Fu Yoga', v: 'OLK4HuRi81w'},
-    { t: 'Contos De Um Caçador de Sombras', v: 'Icmqof2rfHg'},
+    { t: 'Contos Do Caçador de Sombras', v: 'Icmqof2rfHg'},
 ];
 
 
