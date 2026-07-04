099d4772c3dd58920746e43ad55cb69c9e2ca3c9
FanWiki <148832529+Mister-c21@users.noreply.github.com>
2026-07-04 00:02:02 -0300

    Update dataFilmes.js

--- pages/More/Entretenimento/dataFilmes.js
+++ pages/More/Entretenimento/dataFilmes.js
@@ -1,6 +1,7 @@
 const dataFilmes = [
     { t: '3 Espiães Demais O Filme', v: 'zj3f0uvaLEU' }, 
-    { t: 'A Lenda Da Serpente', v: 'pjyih9cmfKk' }
+    { t: 'A Lenda Da Serpente', v: 'pjyih9cmfKk' },
+    { t: 'Operação Zodíaco', v: 'EPZzk-H0iew'},
 ];
 
 
