cb04d437069172ae58f68d584bac824df058ab19
FanWiki <148832529+Mister-c21@users.noreply.github.com>
2026-07-04 00:15:56 -0300

    Update dataFilmes.js

--- pages/More/Entretenimento/dataFilmes.js
+++ pages/More/Entretenimento/dataFilmes.js
@@ -1,9 +1,10 @@
 const dataFilmes = [
-    { t: '3 Espiães Demais O Filme', v: 'zj3f0uvaLEU' }, 
-    { t: 'A Lenda Da Serpente', v: 'pjyih9cmfKk' },
+    { t: '3 Espiães Demais O Filme', v: 'zj3f0uvaLEU'}, 
+    { t: 'A Lenda Da Serpente', v: 'pjyih9cmfKk'},
     { t: 'Operação Zodíaco', v: 'EPZzk-H0iew'},
     { t: 'Kung Fu Yoga', v: 'OLK4HuRi81w'},
     { t: 'Contos Do Caçador de Sombras', v: 'Icmqof2rfHg'},
+    { t: 'Power Rangers o Filme-1995', v: 'RVrYb7sz_OA'},
 ];
 
 
