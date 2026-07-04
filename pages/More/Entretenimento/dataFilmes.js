f41a146f1bbc592f5ee0d339bcaf39d99c0a9ad0
FanWiki <148832529+Mister-c21@users.noreply.github.com>
2026-07-04 00:07:56 -0300

    Update dataFilmes.js

--- pages/More/Entretenimento/dataFilmes.js
+++ pages/More/Entretenimento/dataFilmes.js
@@ -2,6 +2,8 @@ const dataFilmes = [
     { t: '3 Espiães Demais O Filme', v: 'zj3f0uvaLEU' }, 
     { t: 'A Lenda Da Serpente', v: 'pjyih9cmfKk' },
     { t: 'Operação Zodíaco', v: 'EPZzk-H0iew'},
+    { t: 'Kung Fu Yoga', v: 'OLK4HuRi81w'},
+    { t: 'Contos De Um Caçador de Sombras', v: 'Icmqof2rfHg'},
 ];
 
 
