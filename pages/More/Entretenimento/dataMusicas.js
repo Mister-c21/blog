a002f3b45ae49336fa326872c4f44d0a8a8c65f2
FanWiki <148832529+Mister-c21@users.noreply.github.com>
2026-07-04 08:24:53 -0300

    Update dataMusicas.js

--- pages/More/Entretenimento/dataMusicas.js
+++ pages/More/Entretenimento/dataMusicas.js
@@ -3,6 +3,9 @@ const dataMusicas = [
     { t: 'LexClash-Caminho Da Dor', v: 'tiP5oB6vBEk' },
     { t: 'CaioLuix-Aroma Doce', v: 'Xpv_SKc8GdA'},
     { t: 'Tipo Uma Flor', v: 'CguWMhZGZ_k'},
+    { t: 'Dança Do Caos', v: 'YBqh5iTDGP4'},
+    { t: 'Seu Astro', v: 'pdhCJ3v-7mU'},
+    
 ];
 
 
