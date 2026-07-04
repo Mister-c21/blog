81371df80a035284165336bfd5b899e565fabe5d
FanWiki <148832529+Mister-c21@users.noreply.github.com>
2026-07-04 06:40:15 -0300

    Add new music entries to dataMusicas.js

--- pages/More/Entretenimento/dataMusicas.js
+++ pages/More/Entretenimento/dataMusicas.js
@@ -1,6 +1,8 @@
 const dataMusicas = [
     { t: 'VMZ-COLUMBINA', v: 'uWEuyJ0Epog' }, 
-    { t: 'LexClash-Caminho Da Dor', v: 'tiP5oB6vBEk' }
+    { t: 'LexClash-Caminho Da Dor', v: 'tiP5oB6vBEk' },
+    { t: 'CaioLuix-Aroma Doce', v: 'Xpv_SKc8GdA'},
+    { t: 'Tipo Uma Flor', v: 'CguWMhZGZ_k'},
 ];
 
 
