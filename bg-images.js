6da9bd1b6318065d149fe5c17642e642b723944b
FanWiki <148832529+Mister-c21@users.noreply.github.com>
2026-08-09 15:30:40 -0300

    Create bg-images.js

---
+++ bg-images.js
@@ -0,0 +1,7 @@
+const bgImagesArray = [
+    "https://images8.alphacoders.com/139/thumb-1920-1398232.png",
+    "https://images3.alphacoders.com/139/thumb-1920-1398471.png",
+    "https://images5.alphacoders.com/131/thumb-1920-1319300.jpeg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4E77cFuR7jeBGRc5o-ZlGyx2bHZoPD-v7GVvTHPYDIFB9JGxG726Y2JM&s=10",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRH777Yb7xOhSoZYTeCpWArfCeVLWzt2E--HChLLsriY4wiM7W9TJo2rEP&s=10"
+];
