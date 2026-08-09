0bf3e545c75de45f7b8f652dc620e92b19e2f4bd
FanWiki <148832529+Mister-c21@users.noreply.github.com>
2026-08-09 16:25:05 -0300

    Update bg-images.js

--- bg-images.js
+++ bg-images.js
@@ -1,7 +1,25 @@
-const bgImagesArray = [
+const bgImagesDesktop = [
     "https://images8.alphacoders.com/139/thumb-1920-1398232.png",
     "https://images3.alphacoders.com/139/thumb-1920-1398471.png",
     "https://images5.alphacoders.com/131/thumb-1920-1319300.jpeg",
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4E77cFuR7jeBGRc5o-ZlGyx2bHZoPD-v7GVvTHPYDIFB9JGxG726Y2JM&s=10",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRH777Yb7xOhSoZYTeCpWArfCeVLWzt2E--HChLLsriY4wiM7W9TJo2rEP&s=10"
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRH777Yb7xOhSoZYTeCpWArfCeVLWzt2E--HChLLsriY4wiM7W9TJo2rEP&s=10",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTra6NvZLMM2Axn4TKIc8O0TUB2zvNk09DhUqr6xad4mKfnYYyuJY1iRHq&s=10",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIToPgizLaMQA-IKPIxYVcQHJMwg-KvZWPBzVVJOrW0T1xOrcGJlTTlhzT&s=10",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZFpIWmZrugHgU5MKLuvlkQDJkEnklrlLeEXJ2f8JHvFxQJt5TQ8afbK-Y&s=10",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLM_hhIBORPc-ZFW0cNvOOwSJ3M-dvjWc8My6LID2tqT2-ZusFXW9FaM0u&s=10",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdG_kmBe9gafd1iJARBGGEeargPHU8olO3VgFsbzSCV8c_F65scj9VBQiv&s=10",
+];
+
+
+const bgImagesMobile = [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRreVPZ0-7HClIdZBpMLMWvzOt2vc8FbxxpKEBHMiWWNCNmlotOrrKbNvo&s=10",
+    "https://c4.wallpaperflare.com/wallpaper/44/894/502/yor-forger-spy-x-family-anime-girls-fan-art-hd-wallpaper-preview.jpg",
+    "https://w0.peakpx.com/wallpaper/84/854/HD-wallpaper-anime-spy-x-family-yor-forger.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhM_qNCg5pLox6a7GhnylTGbNhT_AiZaf5e_Q6aVTE0dN9KgpOxU83zVs&s=10",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdxKqxRpX6aCGokvbBWxH8taSfOy20ScHQ4YlOa2PRVT_SjzHx1s2xqhE&s=10",
+    "https://i.pinimg.com/736x/3d/6d/66/3d6d6647783c5b7d31fa43259d541e8b.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTt_8XpQbdA9bFkRtYnnI4wG8VhSmD-E9qVHdtOv2QicRMzAELPi8uFCqx&s=10",
+    "https://motionbgs.com/i/c/720x1280/media/8091/waguri-kaoruko.720x1280.jpg",
+    
 ];
