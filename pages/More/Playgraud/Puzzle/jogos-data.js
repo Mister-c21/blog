1724276b89eba52963d28ea65623fe1ca4868e46
FanWiki <148832529+Mister-c21@users.noreply.github.com>
2026-08-11 10:33:11 -0300

    Update jogos-data.js

--- pages/More/Playgraud/Puzzle/jogos-data.js
+++ pages/More/Playgraud/Puzzle/jogos-data.js
@@ -1,11 +1,11 @@
 // jogos-data.js
-const jogosArray = [
+var jogosArray = [
     {
-        titulo: "", // Deixe em branco para o script puxar automaticamente o título da URL/arquivo, ou digite um manualmente
+        titulo: "", 
         url: "./quebra-cabeca/quebra-cabeca.html",
-        bgImage: "Src/Quebra_Cabeça.png", // Se deixar em branco (""), o script usará a logo do Google via URL
+        bgImage: "Src/Quebra_Cabeça.png",
         icone: "fas fa-puzzle-piece"
     }
-    // Adicione mais jogos aqui no formato:
-     { titulo: "", url: "https://cdn.htmlgames.com/1010Sudoku/", bgImage: "", icone: "fas fa-gamepad" }
+    // Adicione mais jogos aqui se precisar:
+    { titulo: "", url: "https://cdn.htmlgames.com/1010Sudoku/", bgImage: "", icone: "fas fa-gamepad" }
 ];
